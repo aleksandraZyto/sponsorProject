4ec772b6b71518e7f9767fa2bc7a852a18f122bb
Ola Zyto <olazyto@Olas-MacBook-Pro.local>
2023-05-16 09:14:21 +0200

    Added login and register endpoints

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	username string `json:"username"`
+	password string `json:"password"`
+	name     string `json:"name"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+	router.POST("/register", registerHandler)
+	router.POST("/login", loginHandler)
+
+	router.Run("localhost:8080")
+}
+
+func registerHandler(c *gin.Context) {
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		return
+	}
+	c.String(http.StatusOK)
+}
+
+func loginHandler(c *gin.Context) {
+	var username string
+	var password string
+	usernameErr := c.BindJSON(&username)
+	passwordErr := c.BindJSON(&password)
+	if usernameErr != nil || passwordErr != nil {
+		return
+	}
+	c.String(http.StatusOK)
 }
