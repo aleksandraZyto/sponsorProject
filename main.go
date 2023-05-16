3f984d1e3f79700eec8757be131526b4822ded3d
Ola Zyto <olazyto@Olas-MacBook-Pro.local>
2023-05-16 10:25:31 +0200

    Added loginData struct

--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
+type LoginData struct {
 	username string `json:"username"`
 	password string `json:"password"`
-	name     string `json:"name"`
+}
+
+type User struct {
+	loginData LoginData `json:"loginData"`
+	name      string    `json:"name"`
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/register", registerHandler)
 	router.POST("/login", loginHandler)
+	router.GET("/hello", helloHandler)
 
 	router.Run("localhost:8080")
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, "Hello")
 }
 
 func registerHandler(c *gin.Context) {
@@ -25,16 +35,15 @@ func registerHandler(c *gin.Context) {
 	if err := c.BindJSON(&user); err != nil {
 		return
 	}
-	c.String(http.StatusOK)
+	c.String(http.StatusOK, "success")
 }
 
 func loginHandler(c *gin.Context) {
-	var username string
-	var password string
-	usernameErr := c.BindJSON(&username)
-	passwordErr := c.BindJSON(&password)
-	if usernameErr != nil || passwordErr != nil {
+	var loginData LoginData
+	loginErr := c.ShouldBindJSON(&loginData)
+	fmt.Println(loginErr)
+	if loginErr != nil {
 		return
 	}
-	c.String(http.StatusOK)
+	c.String(http.StatusOK, "success")
 }
