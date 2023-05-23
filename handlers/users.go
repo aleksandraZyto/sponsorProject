28e58651f5eec970029267a7b72bc999fa8d95b9
Ola Zyto <olazyto@Olas-MacBook-Pro.local>
2023-05-23 20:33:43 +0200

    Refactored /login endopint handling

--- handlers/users.go
+++ handlers/users.go
@@ -4,6 +4,7 @@ import (
 	database "chat-app/database"
 	models "chat-app/models"
 	"encoding/base64"
+	"errors"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,12 +15,19 @@ type RegisterRequest struct {
 	Password string `json:"password"` // these have to be capital, otherwise body parser wont work
 }
 
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type UserHandler interface {
 	Register(req *RegisterRequest) models.User
+	Login(req *LoginRequest) error // why LG has to be a pointer? apparently non pointer breaks body parsing:/
 }
 
 type UserHandlerStruct struct{}
 
+// IMPLEMENT ERROR TO RETURN
 func (handler *UserHandlerStruct) Register(req *RegisterRequest) models.User {
 	req.Password = base64.StdEncoding.EncodeToString([]byte(req.Password))
 	user := models.User{
@@ -29,6 +37,20 @@ func (handler *UserHandlerStruct) Register(req *RegisterRequest) models.User {
 	}
 	database.DB.Db.Create(&user)
 	return user
+}
+
+func (handler *UserHandlerStruct) Login(req *LoginRequest) error {
+	user := models.User{}
+	if err := database.DB.Db.Where("username = ?", req.Username).First(&user).Error; err != nil {
+		return err
+	}
+
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(req.Password))
+
+	if string(user.Password) != encodedPass {
+		return errors.New("Invalid password")
+	}
+	return nil
 }
 
 func Home(c *fiber.Ctx) error {
--- routes.go
+++ routes.go
@@ -19,5 +19,17 @@ func CreateApp(handler handlers.UserHandler) *fiber.App {
 		return c.Status(200).JSON(user)
 	})
 
+	app.Post("/login", func(c *fiber.Ctx) error {
+		req := new(handlers.LoginRequest)
+		if err := c.BodyParser(req); err != nil {
+			return c.Status(400).SendString("Invalid payload")
+		}
+		handlerErr := handler.Login(req)
+		if handlerErr != nil {
+			return c.Status(500).SendString(handlerErr.Error())
+		}
+		return c.Status(200).SendString("Login successful")
+	})
+
 	return app
 }
