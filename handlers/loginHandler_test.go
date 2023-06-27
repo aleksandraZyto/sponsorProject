c1bc8bb9748698d133c0b85dd3dde97163088d83
Ola Zyto <olazyto@Olas-MacBook-Pro.local>
2023-06-27 13:24:12 +0200

    Change the way of mocking in the test file for the register handler test

--- handlers/loginHandler_test.go
+++ handlers/loginHandler_test.go
@@ -5,23 +5,26 @@ import (
 	"chat-app/models"
 	"chat-app/repos"
 	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-type RegistererStructMock struct {
-	mock.Mock
+var WrapperRegisterMock func(req *models.RegisterRequest, repo repos.UserRepository) (models.User, error)
+
+type RegistererStructMock struct{}
+
+func (rsm RegistererStructMock) WrapperRegister(req *models.RegisterRequest, repo repos.UserRepository) (models.User, error) {
+	return WrapperRegisterMock(req, repo)
 }
 
-func (m *RegistererStructMock) WrapperRegister(_ *models.RegisterRequest, _ repos.UserRepository) (models.User, error) {
-	return models.User{}, nil
-}
-
-func TestRegister_HappyPath(t *testing.T) {
+func TestRegisterHandler_HappyPath(t *testing.T) {
+	WrapperRegisterMock = func(req *models.RegisterRequest, repo repos.UserRepository) (models.User, error) {
+		return models.User{}, nil
+	}
 	registerReq := &models.RegisterRequest{
 		LoginData: models.LoginRequest{
 			Username: "Olka",
@@ -36,11 +39,28 @@ func TestRegister_HappyPath(t *testing.T) {
 	c.Request = req
 
 	RegisterHandler(c, &RegistererStructMock{})
+
 	assert.Equal(t, http.StatusCreated, recorder.Code)
-	//fmt.Println("r body:", recorder.Body)
-	//assert.Equal(t, "", recorder.Body)
 }
 
-// TODO: Sad path
-//args := m.Called(req, repo) TODO: How to use this .Called
-//m.On("WrapperRegister", registerReq, repo).Return("Olkaaaaa", errors.New("so,e error")) TODO: How to use this .Return
+func TestRegisterHandler_BadRequest(t *testing.T) {
+	WrapperRegisterMock = func(req *models.RegisterRequest, repo repos.UserRepository) (models.User, error) {
+		return models.User{}, errors.New("error when creating a user, username might be already taken")
+	}
+	registerReq := &models.RegisterRequest{
+		LoginData: models.LoginRequest{
+			Username: "Olka",
+			Password: "1111",
+		},
+		Name: "Alex",
+	}
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+	jsonValue, _ := json.Marshal(registerReq)
+	req, _ := http.NewRequest("POST", "/register", bytes.NewBuffer(jsonValue))
+	c.Request = req
+
+	RegisterHandler(c, &RegistererStructMock{})
+
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+}
